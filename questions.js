f3413634dd73c7fa5c7ff3e8bd0f0f2dd9f534fe
Barry-sktch <barakah.muluzi@bmis.mw>
2024-11-29 17:05:04 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,80 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does it mean to be an entreprenuer?",
+        answer: "A. a person who sets up a business or businesses, taking on financial risks in the hope of profit.",
+        options: [
+            "A. a person who sets up a business or businesses, taking on financial risks in the hope of profit.",
+            "B. a business man",
+            "C. a person works under an organisation ",
+            "D. a person who is unemployed but still receeives a profit"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the primary goal of a business?",
+        answer: "C. To make a profit",
+        options: [
+            "A. To create jobs",
+            "B. To solve problems",
+            "C. To make a profit",
+            "D. To pay taxes"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following is a key characteristic of an entrepreneur?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is a business plan?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of these is NOT a funding option for startups?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which of these is NOT a funding option for startups?",
+        answer: "C. Communication",
+        options: [
+            "A Financial forecasting", 
+            "B Market research", 
+            "C. Communication", 
+            "A. Coding" 
+        ]
+    },
+    {
+        numb: 7, 
+        question: "Why is customer feedback important in business?" , 
+        answer: "C. It provides insights to improve products and services", 
+        options: [
+            "A. It helps in pricing products higher", 
+            "B. It ensures consistent sales", 
+            "C. It provides insights to improve products and services", 
+            "D. It helps reduce competition"
+        ]
+    }
+    
+];
